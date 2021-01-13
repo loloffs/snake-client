5eeb54e9784f3c45a0694abf472ce9589eef39c7
loloffs <lukeoloffs@gmail.com>
2021-01-13 22:04:22 +0000

    Added control+c to end

--- play.js
+++ play.js
@@ -1,3 +1,30 @@
 const { connect } = require('./client');
+const { stdin } = require('process');
 console.log('Connecting ...');
 connect();
+
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+  
+  return stdin;
+};
+
+
+setupInput();
