8210474df96514abcb72910060186db543bc54d9
loloffs <lukeoloffs@gmail.com>
2021-01-14 00:57:56 +0000

    Made movement commands WASD for snake

--- input.js
+++ input.js
@@ -1,7 +1,6 @@
 const { connect } = require('./client');
 const { stdin } = require('process');
-console.log('Connecting ...');
-connect();
+// connect();
 
 
 /**
@@ -9,13 +8,30 @@ connect();
  * Specifically, so that we can handle user input via stdin
  */
 
+
+let connection;
+
 const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
--- play.js
+++ play.js
@@ -2,6 +2,10 @@ const { connect } = require('./client');
 const { setupInput } = require('./input');
 
 console.log('Connecting ...');
-connect();
+console.log("Move: up - move up one square (unless facing down)");
+console.log("Move: down - move down one square (unless facing up)");
+console.log("Move: left - move left one square (unless facing right)");
+console.log("Move: right - move left one square (unless facing left)");
 
-setupInput();
+
+setupInput(connect());
